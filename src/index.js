ab71ee87d25b7ec7531dbd31354e6f472be5c855
Arseny Skibitsky <googars@gmail.com>
2021-10-02 01:41:16 +0300

    Added react-helmet and possible fix for FloC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Helmet} from "react-helmet";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
